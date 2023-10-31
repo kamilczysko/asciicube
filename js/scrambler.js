5e2027984a2107f0352d9a944ff301aedfa9d59d
Kamil <kjas92@gmail.com>
2023-11-01 00:30:05 +0100

    add scrambe input

---
+++ js/scrambler.js
@@ -0,0 +1,15 @@
+const scrambles = [
+    "L' U D B2 R' U' B D B U2 R' D2 R L' U R2 B2 L' U2 F B' D B2 D L",
+    "D2 R' U2 D2 R2 F2 B' U2 L2 R U' L2 B F L U2 D' F U R U2 F' R' U' R'",
+    "L D' L' D' R' L' U2 R' D L2 R2 B2 R' F B D2 R L U R F U' F D2 R2",
+    "D' B F D' U2 F2 L B2 L' R U2 F' R B2 D2 F2 U L2 B R' U' L D F2 L2",
+    "L' U D B2 R' U' B D B U2 R' D2 R L' U R2 B2 L' U2 F B' D B2 D L"
+]
+
+const randomScramble = parseInt(Math.floor(Math.random() * (scrambles.length-1)))
+console.log(randomScramble)
+document.getElementById("scrambleInput").value = scrambles[randomScramble];
+
+document.getElementById("scrambleOk").onclick = () => {
+    
+}
